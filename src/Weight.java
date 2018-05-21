fd41942b5e65e08cf1cf4ce0dcbdaf273b0c74d5
Chintan Patel <chintanpatel911@gmail.com>
2018-05-21 18:50:34 -0400

    Update Weight.java

--- src/Weight.java
+++ src/Weight.java
@@ -1,9 +1,5 @@
 
-/*
- * Name : Chintan Patel
- * CSU ID : 2473177
- * Assigment 6 : Calculate weight for grade 
- */
+
 public class Weight{
 	public static void main (String[] args) {
 		int [] value = {98, 99, 98, 100, 101, 102, 100, 104, 105, 105, 106, 105, 103, 104, 103, 105, 105, 106, 107, 106, 105, 105, 105, 104, 103, 102, 102, 101, 100, 102};
